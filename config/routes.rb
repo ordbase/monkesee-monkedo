a4b9c890ece49060b566021b5a8c3d02c6fc20c6
Gerald Bauer <gerald.bauer@gmail.com>
2014-01-24 12:24:34 -0600

    up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,21 @@
+
 Prost::Application.routes.draw do
 
-  match 'signin', :to => 'sessions#new'
-  match 'signout', :to => 'sessions#destroy'
-  
-  match 'about', :to => 'pages#about'
-  
-  resource :session, :only => [:new, :create, :destroy]
-  
-  match 'time',   :to => 'time#index'
+  mount About::Server,     :at => '/sysinfo'
+  mount DbBrowser::Server, :at => '/browse'
 
-  ###############################
-  # routes for shortcuts (friendly urls)
-  
-  ## 2 lower case letters - assume shortcut for country
-  match '/:key', :to => 'countries#shortcut', :as => :short_country_worker, :key => /[a-z]{2}/
+  ###
+  # mount sinatra app (bundled w/ sportdb-service gem) for json api service
+
+  # todo: add  JSON API link to layout
+
+  get '/api' => redirect('/api/v1')
+  mount BeerDb::Server, :at => '/api/v1' # NB: make sure to require 'beerdb/server' in env
+
+  ## mount sinatra app (bundled w/ logutils gem)
+  mount LogDb::Server, :at => '/logs'    # NB: make sure to require 'logutils/server' in env
 
 
-  # current user's start page/front page
-  match 'frontpage', :to => 'frontpage#index'
-
-  match 'beers/:beer_id/users/:id',        :to => 'users#add_beer',    :as => 'add_beer_to_user'
-  match 'breweries/:brewery_id/users/:id', :to => 'users#add_brewery', :as => 'add_brewery_to_user'
-  match 'users/:user_id/beers/:beer_id/drinks/new', :to => 'drinks#new', :as => 'new_user_beer_drink'
-
-  match 'bookmarks/:id/flag', :to => 'bookmarks#update_flag', :as => 'update_bookmark_flag'
-
-  resources :drinks
-  resources :bookmarks 
-  
-
-  resources :users do
-   ## resources :drinks
-   ## resources :bookmarks
-  end
 
   #############################
   # routes for mobile / m
@@ -65,18 +48,8 @@ Prost::Application.routes.draw do
     resources :users
   end
 
-  ## mount sinatra app (bundled w/ logutils gem)
-  mount LogDb::Server, :at => '/logs'    # NB: make sure to require 'logutils/server' in env
 
-  match '/api' => redirect('/api/v1')
-  mount BeerDb::Server, :at => '/api/v1' # NB: make sure to require 'beerdb/server' in env
+  mount BeerDbAdmin::Engine, :at => '/'  # mount a root possible?
 
-  resources :beers
-  resources :brands
-  resources :breweries
-  resources :countries
-  resources :regions
-  resources :tags
 
-  root :to => 'beers#index'
 end
--- engine/config/routes.rb
+++ engine/config/routes.rb
@@ -1,4 +1,5 @@
-Prost::Application.routes.draw do
+
+BeerDbAdmin::Engine.routes.draw do
 
   match 'signin', :to => 'sessions#new'
   match 'signout', :to => 'sessions#destroy'
@@ -33,43 +34,6 @@ Prost::Application.routes.draw do
    ## resources :drinks
    ## resources :bookmarks
   end
-
-  #############################
-  # routes for mobile / m
-
-  match 'm', :to => 'mobile/pages#home'
-
-  namespace :mobile do
-    match 'signin',  :to => 'sessions#new'
-    match 'signout', :to => 'sessions#destroy'
-
-    resource :session, :only => [:new, :create, :destroy]
-
-    match 'time',   :to => 'time#index'
-
-    match 'bookmarks/:id/flag', :to => 'bookmarks#update_flag', :as => 'update_bookmark_flag'
-
-    resources :users
-    resources :drinks
-    resources :bookmarks
-    resources :beers
-  end
-
-
-  #############
-  # routes for admin
-
-  match 'admin', :to => 'admin/users#index'
-  
-  namespace :admin do
-    resources :users
-  end
-
-  ## mount sinatra app (bundled w/ logutils gem)
-  mount LogDb::Server, :at => '/logs'    # NB: make sure to require 'logutils/server' in env
-
-  match '/api' => redirect('/api/v1')
-  mount BeerDb::Server, :at => '/api/v1' # NB: make sure to require 'beerdb/server' in env
 
   resources :beers
   resources :brands
